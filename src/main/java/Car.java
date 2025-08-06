3ed906098c0c6a58454502ddac244bc2fdaa8e3e
Fanis <iskhak.fanis@yandex.ru>
2025-08-06 15:56:48 +0300

    Add solution for final sprint 2.

---
+++ src/main/java/Car.java
@@ -0,0 +1,8 @@
+public class Car {
+    public Car(String name, int speed) {
+        this.name = name;
+        this.speed = speed;
+    }
+    String name = "";
+    int speed = 0;
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,37 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Let's race!\nEnter 3 racer names and their speeds:");
+        Scanner scanner = new Scanner(System.in);
+        Race race = new Race();
+        boolean pass;
+        for (int i=1; i<=3; i++) {
+            pass = false;
+            while(!pass) {
+                System.out.println("Enter name of racer #" + i + ":");
+                String name = scanner.nextLine();
+                if(name.isEmpty()) {
+                    System.out.println("Empty name! Please, try again.");
+                    continue;
+                }
+                while(!pass) {
+                    System.out.println("Enter speed of racer #" + i + ":");
+                    double inputSpeed = scanner.nextDouble();
+                    scanner.nextLine();
+                    int speed = (int) inputSpeed;
+                    if ((double) speed != inputSpeed || speed < 0 || speed > 250) {
+                        System.out.println("Invalid speed! Please, try again.");
+                        continue;
+                    }
+                    Car car = new Car(name, speed);
+                    race.updateLeader(car);
+                    pass = true;
+                }
+            }
+        }
+        System.out.println("The winner is: " + race.leader);
+        scanner.close();
+        System.out.println("See you next time!");
     }
 }
